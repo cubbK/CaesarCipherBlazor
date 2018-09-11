f7961384fa08ed368d1a2fc9162278cef50d8d1b
Dan <danpopa1998@gmail.com>
2018-09-12 01:53:05 +0300

    add input fields

---
+++ caesar-chypher-blazor/Logic/CaesarBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+
+public class CaesarBase : BlazorComponent
+{
+
+    public string InputText { get; set; } = "Example String";
+    public int Key { get; set; } = 1;
+    public string OutputText { get; set; } = "";
+}
