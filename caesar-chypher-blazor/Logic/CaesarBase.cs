72027da3506c2698245644d7864b9db83cec5f2d
Dan <danpopa1998@gmail.com>
2018-09-12 21:48:43 +0300

    add improved key changer

--- caesar-chypher-blazor/Logic/CaesarBase.cs
+++ caesar-chypher-blazor/Logic/CaesarBase.cs
@@ -6,4 +6,17 @@ public class CaesarBase : BlazorComponent
     public string InputText { get; set; } = "Example String";
     public int Key { get; set; } = 1;
     public string OutputText { get; set; } = "";
+
+    public void OnDecreaseKeyClick ()
+    {
+        if (Key > 0)
+        {
+            Key = Key - 1;
+        }
+    }
+
+    public void OnIncreaseKeyClick()
+    {
+        Key = Key + 1;
+    }
 }
