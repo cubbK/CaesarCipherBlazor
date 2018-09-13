3a976e50efb6081c543aa8de39f73356a7c1d2ba
Dan <danpopa1998@gmail.com>
2018-09-14 00:53:38 +0300

    Rename Project with PascalCase and unit test project

---
+++ CaesarCipherBlazor.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CaesarCipherBlazor.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
