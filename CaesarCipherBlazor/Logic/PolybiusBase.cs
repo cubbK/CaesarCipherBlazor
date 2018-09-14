d627587639d7eb09fd52bc6d29c1ca3588d5ab50
Dan <danpopa1998@gmail.com>
2018-09-14 23:05:10 +0300

    make polybius page render and appear in nav

---
+++ CaesarCipherBlazor/Logic/PolybiusBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+using System;
+using System.Collections.Generic;
+using Utils;
+
+public class PolybiusBase : BlazorComponent
+{
+    public enum Operations
+    {
+        Decode,
+        Encode
+    }
+
+    public string InputText { get; set; } = "Example String";
+    public int Key { get; set; } = 0;
+    public Operations Operation { get; set; } = Operations.Encode;
+    public string OutputText { get; set; } = null;
+}
