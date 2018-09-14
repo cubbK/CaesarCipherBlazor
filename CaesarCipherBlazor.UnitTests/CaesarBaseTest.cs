00910c30cf44f75ee34798f19c26ee7a17402353
Dan <danpopa1998@gmail.com>
2018-09-14 22:10:29 +0300

    write tests for caesarbase class

--- CaesarCipherBlazor.UnitTests/CaesarBaseTest.cs
+++ CaesarCipherBlazor.UnitTests/CaesarBaseTest.cs
@@ -1,15 +1,65 @@
 using System;
 using Xunit;
 
+
 namespace CaesarCipherBlazor.UnitTests
 {
     public class CaesarBaseTest
     {
         [Fact]
-        public void Test1()
+        public void ConvertInputText_EncodeExampleStringEShift3_SetsOutputTextRight()
         {
-            Console.WriteLine(123);
-            Assert.False(false);
+            var caesarBase = new CaesarBase();
+            caesarBase.InputText = "e";
+            caesarBase.Key = 3;
+            caesarBase.Operation = CaesarBase.Operations.Encode;
+            caesarBase.ConvertInputText();
+            Assert.Equal("b", caesarBase.OutputText);
         }
+
+        [Fact]
+        public void ConvertInputText_EncodeExampleStringEShift10_SetsOutputUString()
+        {
+            var caesarBase = new CaesarBase();
+            caesarBase.InputText = "e";
+            caesarBase.Key = 10;
+            caesarBase.Operation = CaesarBase.Operations.Encode;
+            caesarBase.ConvertInputText();
+            Assert.Equal("u", caesarBase.OutputText);
+        }
+
+        [Fact]
+        public void ConvertInputText_DecodeExampleStringEShift3_SetsOutputHString()
+        {
+            var caesarBase = new CaesarBase();
+            caesarBase.InputText = "e";
+            caesarBase.Key = 3;
+            caesarBase.Operation = CaesarBase.Operations.Decode;
+            caesarBase.ConvertInputText();
+            Assert.Equal("h", caesarBase.OutputText);
+        }
+
+        [Fact]
+        public void ConvertInputText_EncodeExampleStringShift3_SetsOutputTextRight()
+        {
+            var caesarBase = new CaesarBase();
+            caesarBase.InputText = "example";
+            caesarBase.Key = 3;
+            caesarBase.Operation = CaesarBase.Operations.Encode;
+            caesarBase.ConvertInputText();
+            Assert.Equal("buxjmib", caesarBase.OutputText);
+        }
+
+        [Fact]
+        public void ConvertInputText_EncodeExampleStringComplexShift3_SetsOutputTextRight()
+        {
+            var caesarBase = new CaesarBase();
+            caesarBase.InputText = "Exampýle";
+            caesarBase.Key = 3;
+            caesarBase.Operation = CaesarBase.Operations.Encode;
+            caesarBase.ConvertInputText();
+            Assert.Equal("Buxjmýib", caesarBase.OutputText);
+        }
+
     }
 }
